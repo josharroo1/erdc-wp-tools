a60c81a939b2a45c952d495e8eb612f396fe5a6b
Josh Arruda <josh.s.arruda@erdc.dren.mil>
2024-03-18 16:42:47 -0500

    fix error

--- includes/admin/cac-auth-admin-functions.php
+++ includes/admin/cac-auth-admin-functions.php
@@ -22,23 +22,13 @@ function cac_auth_render_custom_fields() {
         <thead>
             <tr>
                 <th>Field Label</th>
-                <th>Field Type</th>
-                <th>Options (for Select fields)</th>
                 <th>Actions</th>
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($custom_fields as $field_id => $field_data) : ?>
+            <?php foreach ($custom_fields as $field_id => $field_label) : ?>
                 <tr>
-                    <td><input type="text" name="cac_auth_registration_fields[<?php echo esc_attr($field_id); ?>][label]" value="<?php echo esc_attr($field_data['label']); ?>"></td>
-                    <td>
-                        <select name="cac_auth_registration_fields[<?php echo esc_attr($field_id); ?>][type]">
-                            <option value="text" <?php selected($field_data['type'], 'text'); ?>>Text</option>
-                            <option value="number" <?php selected($field_data['type'], 'number'); ?>>Number</option>
-                            <option value="select" <?php selected($field_data['type'], 'select'); ?>>Select</option>
-                        </select>
-                    </td>
-                    <td><input type="text" name="cac_auth_registration_fields[<?php echo esc_attr($field_id); ?>][options]" value="<?php echo esc_attr($field_data['options']); ?>" placeholder="key1:value1,key2:value2"></td>
+                    <td><input type="text" name="cac_auth_registration_fields[<?php echo esc_attr($field_id); ?>]" value="<?php echo esc_attr($field_label); ?>"></td>
                     <td><button type="button" class="button button-secondary cac-auth-remove-field">Remove</button></td>
                 </tr>
             <?php endforeach; ?>
