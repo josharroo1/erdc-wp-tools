9b703bb591c581398095349fe42ff697c3710baa
Josh Arruda <josh.s.arruda@erdc.dren.mil>
2024-03-21 11:59:31 -0500

    attempt fix for csv

--- cac-auth-plugin.php
+++ cac-auth-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP CAC Sync (DoD)
  * Plugin URI: https://github.com/josharroo1/WP-DoD-CAC-User
  * Description: A WordPress plugin for CAC authentication and user synchronization for the DoD.
- * Version: 2.4.7
+ * Version: 2.4.8
  * Author: Josh Arruda
  * Author URI: https://github.com/josharroo1/WP-DoD-CAC-User
  * License: GPL-2.0+
@@ -16,7 +16,7 @@ if (!defined('WPINC')) {
 }
 
 // Define plugin constants
-define('CAC_AUTH_PLUGIN_VERSION', '2.4.7');
+define('CAC_AUTH_PLUGIN_VERSION', '2.4.8');
 define('CAC_AUTH_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('CAC_AUTH_PLUGIN_URL', plugin_dir_url(__FILE__));
 
