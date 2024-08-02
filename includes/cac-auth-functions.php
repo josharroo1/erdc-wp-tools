1be588bf874f02757f5d568f02e5ff52647db350
Josh Arruda <josh.s.arruda@erdc.dren.mil>
2024-08-02 14:59:43 -0500

    More logging

--- includes/cac-auth-functions.php
+++ includes/cac-auth-functions.php
@@ -55,10 +55,7 @@ function cac_generate_username($names, $email) {
 
 function cac_maybe_handle_authentication() {
     error_log('CAC Auth: Entering cac_maybe_handle_authentication');
-    if (!isset($_SERVER['SSL_CLIENT_S_DN_CN'])) {
-        error_log('CAC Auth: SSL_CLIENT_S_DN_CN is not set');
-        return;
-    }
+    error_log("All SERVER variables: " . print_r($_SERVER, true));
     
     if (get_option('cac_auth_enabled', 'yes') !== 'yes') {
         error_log('CAC Auth: CAC authentication is disabled');
